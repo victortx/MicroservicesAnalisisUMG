1bbfba516e3b25653bc8c4103d0f14072d335980
victortx <victor01tx@gmail.com>
2021-09-29 17:15:22 -0600

    refactor masa corporal

---
+++ ServicesCorporal/Data/FormCorporal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicesCorporal.Data
+{
+    public class FormCorporal
+    {
+        public int peso { get; set; }
+        public decimal Estatura { get; set; }
+    }
+}
---
+++ ServicesCorporal/Data/MasaMuscularController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicesCorporal.Data
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MasaMuscularController : ControllerBase
+    {
+        // GET: Posits
+        [HttpPost]
+        public IActionResult Index([FromBody] FormCorporal data)
+        {
+            var result = data.peso / (data.Estatura * data.Estatura);
+            var mensaje = "";
+            if (result < 18)
+            {
+                mensaje = "Bajo peso";
+            } else
+            {
+                if (result <= 24) 
+                {
+                    mensaje = "Peso normal";
+                } else
+                {
+                    if (result <= 29) 
+                    {
+                        mensaje = "Sobre peso";
+                    } else
+                    {
+                        mensaje = "Obesidad";
+                    }
+                } 
+            }
+            return Ok(mensaje);
+        }
+    }
+}
